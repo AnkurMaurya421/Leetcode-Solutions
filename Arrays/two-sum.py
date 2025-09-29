23ff7f5ae43a62b0c977ca46fb98ac7ad0bb11dd
AnkurMaurya421 <ankurmaurya421@gmail.com>
2025-09-29 15:17:37 +0530

    Added code of two sum problem in python and updated readme file.

---
+++ Arrays/two-sum.py
@@ -0,0 +1,14 @@
+# Problem: Two Sum
+# Link: https://leetcode.com/problems/two-sum/
+# Difficulty: Easy
+# Approach: Use hashmap to store seen numbers and find complement
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap={}
+        for i , num in enumerate(nums):
+            complement=target-num
+            if complement in hashmap:
+                return [hashmap[complement],i]
+            hashmap[num]=i
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Leetcode-Solutions
 Repo to summarise all my leetcode submissions and approach.
+This repository contains my solutions to LeetCode problems, organized by topic.  
+Each problem includes a **link**, **difficulty**, and **approach summary**.
+---
+## Arrays
+1. [Two Sum](https://leetcode.com/problems/two-sum/) - Easy - Hashmap solution
